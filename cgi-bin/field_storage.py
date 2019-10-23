60fa01288cddffad56bb6232f58a07562a63a324
sallysy8 <sallysy8@hotmail.com>
2019-10-22 17:09:45 -0700

    Add cgi exercises

---
+++ cgi-bin/field_storage.py
@@ -0,0 +1,10 @@
+import cgi
+
+print("Content-Type: text/plain")
+print("")
+
+form = cgi.FieldStorage()
+stringval = form.getvalue('a', None)
+listval = form.getlist('b')
+
+print("a: {}, b: {}".format(str(stringval), str(listval)))
